53959e0b6aafc65dd004c6748de706b4262ed8db
Jeff Lueck <lueckcfd@yahoo.com>
2017-08-14 20:59:11 -0500

    style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,15 +2,46 @@ const React = require('react');
 const {Link, IndexLink} = require('react-router');
 const ActiveLink = require('ActiveLink');
 
-var Nav = () => {
-  return(
-    <div>
-      <h2>Nav Component</h2>
-      <ActiveLink to="/">Get Weather</ActiveLink>
-      <ActiveLink to="/about">About</ActiveLink>
-      <ActiveLink to="/examples">Examples</ActiveLink>
-    </div>
-  )
+class Nav extends React.Component {
+  onSearch = (e) => {
+    e.preventDefault();
+    alert('Not yet wired up!');
+  }
+  render() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <ActiveLink to="/">Get Weather</ActiveLink>
+            </li>
+            <li>
+              <ActiveLink to="/about">About</ActiveLink>
+            </li>
+            <li>
+              <ActiveLink to="/examples">Examples</ActiveLink>
+            </li>
+
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search Weather"/>
+                <li>
+                  <input type="submit" className="button" value="Get Weather"/>
+                </li>
+              </li>
+
+            </ul>
+
+          </form>
+        </div>
+      </div>
+    )
+  }
 };
 
 module.exports = Nav;
--- public/bundle.js
+++ public/bundle.js
@@ -28414,6 +28414,14 @@ module.exports = Main;
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 var React = __webpack_require__(4);
 
 var _require = __webpack_require__(61),
@@ -28422,32 +28430,102 @@ var _require = __webpack_require__(61),
 
 var ActiveLink = __webpack_require__(259);
 
-var Nav = function Nav() {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h2',
-      null,
-      'Nav Component'
-    ),
-    React.createElement(
-      ActiveLink,
-      { to: '/' },
-      'Get Weather'
-    ),
-    React.createElement(
-      ActiveLink,
-      { to: '/about' },
-      'About'
-    ),
-    React.createElement(
-      ActiveLink,
-      { to: '/examples' },
-      'Examples'
-    )
-  );
-};
+var Nav = function (_React$Component) {
+  _inherits(Nav, _React$Component);
+
+  function Nav() {
+    var _ref;
+
+    var _temp, _this, _ret;
+
+    _classCallCheck(this, Nav);
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Nav.__proto__ || Object.getPrototypeOf(Nav)).call.apply(_ref, [this].concat(args))), _this), _this.onSearch = function (e) {
+      e.preventDefault();
+      alert('Not yet wired up!');
+    }, _temp), _possibleConstructorReturn(_this, _ret);
+  }
+
+  _createClass(Nav, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'top-bar' },
+        React.createElement(
+          'div',
+          { className: 'top-bar-left' },
+          React.createElement(
+            'ul',
+            { className: 'menu' },
+            React.createElement(
+              'li',
+              { className: 'menu-text' },
+              'React Weather App'
+            ),
+            React.createElement(
+              'li',
+              null,
+              React.createElement(
+                ActiveLink,
+                { to: '/' },
+                'Get Weather'
+              )
+            ),
+            React.createElement(
+              'li',
+              null,
+              React.createElement(
+                ActiveLink,
+                { to: '/about' },
+                'About'
+              )
+            ),
+            React.createElement(
+              'li',
+              null,
+              React.createElement(
+                ActiveLink,
+                { to: '/examples' },
+                'Examples'
+              )
+            )
+          )
+        ),
+        React.createElement(
+          'div',
+          { className: 'top-bar-right' },
+          React.createElement(
+            'form',
+            { onSubmit: this.onSearch },
+            React.createElement(
+              'ul',
+              { className: 'menu' },
+              React.createElement(
+                'li',
+                null,
+                React.createElement('input', { type: 'search', placeholder: 'Search Weather' }),
+                React.createElement(
+                  'li',
+                  null,
+                  React.createElement('input', { type: 'submit', className: 'button', value: 'Get Weather' })
+                )
+              )
+            )
+          )
+        )
+      );
+    }
+  }]);
+
+  return Nav;
+}(React.Component);
+
+;
 
 module.exports = Nav;
 
