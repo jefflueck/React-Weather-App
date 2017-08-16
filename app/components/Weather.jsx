75c52b4cd0c655f6bf39015ee1671c65bbe4fb20
Jeff Lueck <lueckcfd@yahoo.com>
2017-08-16 07:37:55 -0500

    current app state

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,6 +2,7 @@ const React = require ('react');
 const WeatherForm = require('WeatherForm');
 const WeatherMessage = require('WeatherMessage');
 const openWeatherMap = require('openWeatherMap');
+const ErrorModal = require('ErrorModal');
 
 class Weather extends React.Component {
   constructor(props){
@@ -13,7 +14,10 @@ class Weather extends React.Component {
   handleSearch = (location) => {
     let that = this;
 
-    that.setState({isLoading: true});
+    that.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
 
     openWeatherMap.getTemp(location).then((temp) => {
       that.setState ({
@@ -21,28 +25,38 @@ class Weather extends React.Component {
         temp: temp,
         isLoading: false
       })
-    }, ((errorMessage) => {
-      that.setState({isLoading: false});
-      alert("That is not a valid city, please try again.");
-    })
-  )
-};
+    }, function (e) {
+      that.setState({
+        isLoading: false,
+        errorMessage: e.message
+      });
+    });
+  }
   render(){
     // ES6 Destructuring
-    var {isLoading, temp, location} = this.state;
+    var {isLoading, temp, location, errorMessage} = this.state;
 
-    function renderMessage() {
+    renderMessage = () => {
         if (isLoading) {
           return<h3 className="text-center">Fetching Weather...</h3>
       } else if (temp && location) {
           return <WeatherMessage temp={temp} location={location}/>;
       }
-    };
+    }
+
+    renderError = () => {
+      if (typeof errorMessage === 'string') {
+        return (
+          <ErrorModal/>
+        )
+      }
+    }
     return(
       <div>
         <h1 className="text-center">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
+        {renderError()}
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,8 @@ module.exports = {
       About: path.resolve(__dirname, 'app/components/About.jsx'),
       Examples: path.resolve(__dirname, 'app/components/Examples.jsx'),
       ActiveLink: path.resolve(__dirname, 'app/components/ActiveLink.jsx'),
-      openWeatherMap: path.resolve(__dirname, 'app/api/openWeatherMap.jsx')
+      openWeatherMap: path.resolve(__dirname, 'app/api/openWeatherMap.jsx'),
+      ErrorModal: path.resolve(__dirname, 'app/components/ErrorModal.jsx')
     },
     extensions: ['*', '.js', '.jsx']
   },
